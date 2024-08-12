1954bd7bebf1f6eaefd63534163e2ab1e28a3266
manuel-coder 02 <sotundeemmanuel@gmail.com>
2024-08-12 04:40:51 +0100

    finished the slider effect

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,11 @@
 'use strict';
+// Slider Effect
+// const sliderEL = document.querySelector('#range');
+// console.log(sliderEL);
+
+// const sliderProgress = function () {
+// 	const sliderValue = sliderEL.value;
+// 	sliderEL.style.background = `linear-gradient(to right, #f50 ${sliderValue}%, #ccc ${sliderValue}%)`;
+// };
+
+// sliderProgress();
